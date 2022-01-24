133d9b6c69f678d215b2de195c19b47f914ac15a
hyewonShin <rusy7225@naver.com>
2022-01-24 19:59:12 +0900

    글 읽는 페이지 구성하기 글을 읽는 페이지를 구성한다. 글 목록에서 글을 클릭하면 글 읽는 페이지가 나타나도록 구현한다. 글을 새로 작성하였을 때 작성된 글을 읽는 페이지가 나타나도록 구현한다.

--- BoardProject/src/kr/co/hyewon/mapper/BoardMapper.java
+++ BoardProject/src/kr/co/hyewon/mapper/BoardMapper.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 
 import kr.co.hyewon.bean.ContentBean;
 
 public interface BoardMapper {
 
+	@SelectKey(statement = "select content_seq.nextval from dual", keyProperty = "content_idx", before = true, resultType = int.class)
+	
 	// 글 작성 페이지 정보를 서버에 저장 
 	@Insert("insert into content_table(content_idx, content_subject, content_text, " +
 			"content_file, content_writer_idx, content_board_idx, content_date) " +
-			"values (content_seq.nextval, #{content_subject}, #{content_text}, #{content_file, jdbcType=VARCHAR}, " +
+			"values (#{content_idx}, #{content_subject}, #{content_text}, #{content_file, jdbcType=VARCHAR}, " +
 			"#{content_writer_idx}, #{content_board_idx}, sysdate)")
 	void addContentInfo(ContentBean writeContentBean);
 	
@@ -26,7 +29,7 @@ public interface BoardMapper {
 			"where board_info_idx = #{board_info_idx}")
 	String getBaordInfoName(int board_info_idx);
 	
-	// 글목록 출력 
+	// 글목록 데이터 출력 
 	@Select("select a1.content_idx, a1.content_subject, a2.user_name as content_writer_name, " +
 			"		to_char(a1.content_date,'YYYY-MM-DD') as content_date " + 
 			"from content_table a1, user_table a2 " + 
@@ -34,8 +37,34 @@ public interface BoardMapper {
 			"    and a1.content_board_idx = #{board_info_idx} " + 
 			"order by a1.content_idx desc")
 	List<ContentBean> getContentList(int board_info_idx);
+
+	// 글 읽는 페이지 데이터 출력 
+	@Select("select a2.user_name as content_writer_name, " + 
+			"       to_char(a1.content_date, 'YYYY-MM-DD') as content_date, " + 
+			"       a1.content_subject, a1.content_text, a1.content_file " + 
+			"from content_table a1, user_table a2 " + 
+			"where a1.content_writer_idx = a2.user_idx " + 
+			"      and content_idx = #{content_idx}")
+	ContentBean getContentInfo(int content_idx);
+
 }
 
 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 	
